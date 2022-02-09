fb9d3474006cd3c9333385450f18a7d59a6b1b78
Mrwinter <ivan.winter09@gmail.com>
2022-02-09 23:46:38 +0100

    push

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
-console.log('Hallo daar!');
+import axios from 'axios';
+
+async function fetchData() {
+    const listUno = document.getElementById('list_u')
+
+    const countryList = document.createElement('li')
+
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all')
+        console.log(result.data);
+        console.log(result.data[0]);
+
+        result.data.map((allCountries) => {
+            countryList.innerHTML = `
+        
+            <h3>${allCountries.name}</h3>
+            <p>${allCountries.population}</p>
+            `
+
+            listUno.appendChild(countryList)
+        })
+
+
+
+
+
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+fetchData();
+
